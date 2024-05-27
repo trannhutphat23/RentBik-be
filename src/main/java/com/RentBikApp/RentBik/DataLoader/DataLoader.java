e566c2fdfeaccdee6a435600327b75fc6bd6fa14
trannhutphat23 <trannhutphattv@gmail.com>
2024-05-27 14:52:43 +0700

    update fix DataLoader init for gplx

--- src/main/java/com/RentBikApp/RentBik/DataLoader/DataLoader.java
+++ src/main/java/com/RentBikApp/RentBik/DataLoader/DataLoader.java
@@ -8,11 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
     private final GplxRepository gplxRepository;
+    private List<String> gplxs = List.of("A1", "A2", "A3", "A4", "B1", "B2", "C", "D", "E", "F");
 
     @Autowired
     public DataLoader(AccountRepository accountRepository, GplxRepository gplxRepository) {
@@ -29,45 +35,11 @@ public class DataLoader implements CommandLineRunner {
         }
 
         if (gplxRepository.count() == 0){
-            Gplx gplxA1 = new Gplx();
-            gplxA1.setRank("A1");
-            gplxRepository.save(gplxA1);
-
-            Gplx gplxA2 = new Gplx();
-            gplxA2.setRank("A2");
-            gplxRepository.save(gplxA2);
-
-            Gplx gplxA3 = new Gplx();
-            gplxA3.setRank("A3");
-            gplxRepository.save(gplxA3);
-
-            Gplx gplxA4 = new Gplx();
-            gplxA4.setRank("A4");
-            gplxRepository.save(gplxA4);
-
-            Gplx gplxB1 = new Gplx();
-            gplxB1.setRank("B1");
-            gplxRepository.save(gplxB1);
-
-            Gplx gplxB2 = new Gplx();
-            gplxB2.setRank("B2");
-            gplxRepository.save(gplxB2);
-
-            Gplx gplxC = new Gplx();
-            gplxC.setRank("C");
-            gplxRepository.save(gplxC);
-
-            Gplx gplxD = new Gplx();
-            gplxD.setRank("D");
-            gplxRepository.save(gplxD);
-
-            Gplx gplxE = new Gplx();
-            gplxE.setRank("E");
-            gplxRepository.save(gplxE);
-
-            Gplx gplxF = new Gplx();
-            gplxF.setRank("F");
-            gplxRepository.save(gplxF);
+            for (String item : gplxs){
+                Gplx gplx = new Gplx();
+                gplx.setRank(item);
+                gplxRepository.save(gplx);
+            }
         }
     }
 }
